f91b69c54ea51386e853b0f30d04d83dea4464b0
LeightonGuang <leighton.guang@icloud.com>
2025-05-01 04:28:45 +0100

    Feat: New minimize icon

--- src/assets/Icons.tsx
+++ src/assets/Icons.tsx
@@ -10,9 +10,9 @@ export const ExpandIconSvg = ({ ...props }) => (
     <path
       d="M14 10L21 3M21 3H15M21 3V9M10 14L3 21M3 21H9M3 21L3 15"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
@@ -29,9 +29,28 @@ export const CloseIconSvg = ({ ...props }) => (
     <path
       d="M17 7L7 17M7 7L17 17"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export const MinimizeIconSvg = ({ ...props }) => (
+  <svg
+    width="100%"
+    height="100%"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M4 14H10M10 14V20M10 14L3 21M20 10H14M14 10V4M14 10L21 3"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
