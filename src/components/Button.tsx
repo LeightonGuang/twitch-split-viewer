c8b8328a01628ce1165f924b31832196392211f1
LeightonGuang <leighton.guang@icloud.com>
2025-05-01 02:32:53 +0100

    Feat: Create Twitch style button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface CustomButtonProps {
+  children?: React.ReactNode;
+  className?: string;
+  onClick: () => void;
+}
+
+const Button: React.FC<CustomButtonProps> = ({
+  children,
+  className,
+  onClick,
+}) => {
+  return (
+    <button
+      className={`cursor-pointer rounded-sm bg-[#2a292e] px-2! py-[0.3125rem]! text-[0.8125rem] font-semibold text-white hover:bg-[#302f35] ${className}`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
