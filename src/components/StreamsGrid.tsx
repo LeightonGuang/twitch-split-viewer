4959e3c702ddc4295a73acc204cc9b58eea974b3
LeightonGuang <leighton.guang@icloud.com>
2025-05-01 02:36:04 +0100

    Feat: Outline the twitch player of the selected streamer chat

--- src/components/StreamsGrid.tsx
+++ src/components/StreamsGrid.tsx
@@ -5,14 +5,14 @@ const StreamsGrid = ({
   className,
   streamers,
   setStreamers,
+  selectedStreamer,
 }: {
   className?: string;
   streamers: string[];
   setStreamers: React.Dispatch<React.SetStateAction<string[]>>;
+  selectedStreamer: string;
 }) => {
-  const [gridClass, setGridClass] = useState<string>(
-    "grid-cols-1 grdid-rows-1",
-  );
+  const [gridClass, setGridClass] = useState<string>("grid-cols-1 grid-rows-1");
 
   useEffect(() => {
     const handleStreamersChange = () => {
@@ -24,9 +24,11 @@ const StreamsGrid = ({
         setGridClass("grid-cols-2 grid-rows-2");
       } else if (streamers.length === 5 || streamers.length === 6) {
         setGridClass("grid-cols-3 grid-rows-2");
-      } else if (streamers.length === 7 || streamers.length === 8) {
-        setGridClass("grid-cols-4 grid-rows-2");
-      } else if (streamers.length === 9) {
+      } else if (
+        streamers.length === 7 ||
+        streamers.length === 8 ||
+        streamers.length === 9
+      ) {
         setGridClass("grid-cols-3 grid-rows-3");
       } else if (streamers.length === 10) {
         setGridClass("grid-cols-4 grid-rows-3");
@@ -41,8 +43,8 @@ const StreamsGrid = ({
       <div className={`grid h-dvh w-full ${gridClass}`}>
         {streamers.map((streamer, streamerIndex) => (
           <TwitchStreamPlayer
-            className="w-ful h-full"
-            key={streamer}
+            className={`h-full w-full ${selectedStreamer === streamer && "border border-[#9147ff]"}`}
+            key={streamerIndex}
             streamerIndex={streamerIndex}
             streamer={streamer}
             setStreamers={setStreamers}
