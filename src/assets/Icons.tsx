97ae4f998fa2d733d57c3ced0dd28f44074af5e2
LeightonGuang <leighton.guang@icloud.com>
2025-05-08 04:45:39 +0100

    Refactor: Update icons to use Twitch original icon

--- src/assets/Icons.tsx
+++ src/assets/Icons.tsx
@@ -75,45 +75,19 @@ export const PlusIconSvg = ({ ...props }) => (
 );
 
 export const MinimizeSidebarSvg = ({ ...props }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    width="100%"
-    height="100%"
-    viewBox="0 0 24 24"
-    {...props}
-  >
+  <svg width="100%" height="100%" version="1.1" viewBox="0 0 20 20" {...props}>
     <path
-      stroke="currentColor"
-      strokeWidth="1.5"
-      d="M7.22 14.47L9.69 12 7.22 9.53a.75.75 0 111.06-1.06l3 3a.75.75 0 010 1.06l-3 3a.75.75 0 01-1.06-1.06z"
-    />
-    <path
-      stroke="currentColor"
-      strokeWidth="1.5"
-      d="M3.75 2A1.75 1.75 0 002 3.75v16.5c0 .966.784 1.75 1.75 1.75h16.5A1.75 1.75 0 0022 20.25V3.75A1.75 1.75 0 0020.25 2H3.75zM3.5 3.75a.25.25 0 01.25-.25H15v17H3.75a.25.25 0 01-.25-.25V3.75zm13 16.75v-17h3.75a.25.25 0 01.25.25v16.5a.25.25 0 01-.25.25H16.5z"
+      d="M4 16V4H2v12h2zm9-1-1.5-1.5L14 11H6V9h8l-2.5-2.5L13 5l5 5-5 5z"
+      fill="currentColor"
     />
   </svg>
 );
 
 export const ExpandSidebarSvg = ({ ...props }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    width="100%"
-    height="100%"
-    viewBox="0 0 24 24"
-    {...props}
-  >
+  <svg width="100%" height="100%" viewBox="0 0 20 20" {...props}>
     <path
-      stroke="currentColor"
-      strokeWidth="1.5"
-      d="M11.28 9.53L8.81 12l2.47 2.47a.75.75 0 11-1.06 1.06l-3-3a.75.75 0 010-1.06l3-3a.75.75 0 111.06 1.06z"
-    />
-    <path
-      stroke="currentColor"
-      strokeWidth="1.5"
-      d="M3.75 2A1.75 1.75 0 002 3.75v16.5c0 .966.784 1.75 1.75 1.75h16.5A1.75 1.75 0 0022 20.25V3.75A1.75 1.75 0 0020.25 2H3.75zM3.5 3.75a.25.25 0 01.25-.25H15v17H3.75a.25.25 0 01-.25-.25V3.75zm13 16.75v-17h3.75a.25.25 0 01.25.25v16.5a.25.25 0 01-.25.25H16.5z"
+      d="M16 16V4h2v12h-2zM6 9l2.501-2.5-1.5-1.5-5 5 5 5 1.5-1.5-2.5-2.5h8V9H6z"
+      fill="currentColor"
     />
   </svg>
 );
