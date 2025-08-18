25dbd94ffebb3408e369b567f2d83a55357e005c
LeightonGuang <leighton.guang@icloud.com>
2025-08-18 07:34:51 +0100

    Fix: Chat spacing with new button style

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -27,7 +27,7 @@ const Chat = ({
   return (
     <div
       className={`${className} flex h-dvh max-w-[21.25rem] min-w-[3rem] flex-col bg-[#18181a] transition-[width] duration-750 ease-in-out ${
-        showChat ? "w-[30rem]" : "w-[3rem]"
+        showChat ? "w-[30rem]" : "w-[3.75rem]"
       }`}
     >
       <div className="flex h-min w-full flex-wrap gap-2 p-2 text-white">
@@ -53,9 +53,9 @@ const Chat = ({
         {showChat && !selectedExpandedStream && (
           <>
             {streamers.map((streamer, i) => (
-              <button
+              <Button
                 key={streamer}
-                className={`rounded-sm bg-[#2a292e] px-2 py-[0.3125rem] text-[0.8125rem] font-semibold ${streamer && "cursor-pointer hover:bg-[#302f35]"} ${selectedStreamer === streamer && "bg-[#9147ff] hover:bg-[#772CE8]"}`}
+                className={`bg-[#2a292e] font-semibold ${streamer && "cursor-pointer hover:bg-[#302f35]"} ${selectedStreamer === streamer && "bg-[#9147ff] hover:bg-[#772CE8]"}`}
                 disabled={!streamer}
                 title={streamer && streamer + "'s chat"}
                 onClick={() => setSelectedStreamer(streamer)}
@@ -65,7 +65,7 @@ const Chat = ({
                 ) : (
                   <span className="opacity-40">{"Channel " + (i + 1)}</span>
                 )}
-              </button>
+              </Button>
             ))}
 
             {streamers.length < 12 && (
