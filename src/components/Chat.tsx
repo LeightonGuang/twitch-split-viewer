bd8cb058a4ff3fc2ebfa513564a36c82666d4f21
LeightonGuang <leighton.guang@icloud.com>
2025-08-21 04:55:22 +0100

    Feat: New team mode chat select for both teams

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -4,26 +4,41 @@ import {
   MinimizeSidebarSvg,
 } from "../assets/Icons";
 import Button from "./Button";
+import { useState } from "react";
 
 const Chat = ({
   className,
   streamers,
   setStreamers,
+  team1Streamers,
+  setTeam1Streamers,
+  team2Streamers,
+  setTeam2Streamers,
   showChat,
   setShowChat,
-  selectedStreamer,
-  setSelectedStreamer,
+  selectedStreamerChat,
+  setSelectedStreamerChat,
   selectedExpandedStream,
 }: {
   className?: string;
   streamers: string[];
   setStreamers: React.Dispatch<React.SetStateAction<string[]>>;
+  team1Streamers: string[];
+  setTeam1Streamers: React.Dispatch<React.SetStateAction<string[]>>;
+  team2Streamers: string[];
+  setTeam2Streamers: React.Dispatch<React.SetStateAction<string[]>>;
   showChat: boolean;
   setShowChat: React.Dispatch<React.SetStateAction<boolean>>;
-  selectedStreamer: string;
-  setSelectedStreamer: (streamer: string) => void;
+  selectedStreamerChat: string;
+  setSelectedStreamerChat: (streamer: string) => void;
   selectedExpandedStream: string;
 }) => {
+  const [showChatSelect, setShowChatSelect] = useState<boolean>(true);
+
+  const handleShowChatSelectOnClick = () => {
+    setShowChatSelect(!showChatSelect);
+  };
+
   return (
     <div
       className={`${className} flex h-dvh max-w-[21.25rem] min-w-[3rem] flex-col bg-[#18181a] transition-[width] duration-750 ease-in-out ${
@@ -50,46 +65,144 @@ const Chat = ({
           </Button>
         )}
 
-        {showChat && !selectedExpandedStream && (
-          <>
-            {streamers.map((streamer, i) => (
-              <Button
-                key={streamer}
-                className={`bg-[#2a292e] font-semibold ${streamer && "cursor-pointer hover:bg-[#302f35]"} ${selectedStreamer === streamer && "bg-[#9147ff] hover:bg-[#772CE8]"}`}
-                disabled={!streamer}
-                title={streamer && streamer + "'s chat"}
-                onClick={() => setSelectedStreamer(streamer)}
-              >
-                {streamer ? (
-                  <span className="text-white">{streamer}</span>
-                ) : (
-                  <span className="opacity-40">{"Channel " + (i + 1)}</span>
-                )}
-              </Button>
-            ))}
+        <div
+          // TODO: Add animation
+          className={`flex h-full overflow-hidden duration-750 ease-in-out ${showChatSelect ? "max-h-full" : "hidden"}`}
+        >
+          {showChat &&
+            !selectedExpandedStream &&
+            (streamers.length > 0 ? (
+              <div className="flex gap-2">
+                {streamers.map((streamer, i) => (
+                  <Button
+                    key={streamer}
+                    className={`bg-[#2a292e] font-semibold ${streamer && "cursor-pointer hover:bg-[#302f35]"} ${selectedStreamerChat === streamer && "bg-[#9147ff] hover:bg-[#772CE8]"}`}
+                    disabled={!streamer}
+                    title={streamer && streamer + "'s chat"}
+                    onClick={() => setSelectedStreamerChat(streamer)}
+                  >
+                    {streamer ? (
+                      <span className="text-white">{streamer}</span>
+                    ) : (
+                      <span className="opacity-40">{"Channel " + (i + 1)}</span>
+                    )}
+                  </Button>
+                ))}
 
-            {streamers.length < 12 && (
-              <Button
-                onClick={() => {
-                  if (streamers.length < 12) {
-                    setStreamers([...streamers, ""]);
-                  }
-                }}
-                title="Add Channel"
-              >
-                <div className="flex items-center gap-1">
-                  <PlusIconSvg className="h-4 w-4" />
+                {streamers.length < 12 && (
+                  <Button
+                    onClick={() => {
+                      if (streamers.length < 12) {
+                        setStreamers([...streamers, ""]);
+                      }
+                    }}
+                    title="Add Channel"
+                  >
+                    <div className="flex items-center gap-1">
+                      <PlusIconSvg className="h-4 w-4" />
+                    </div>
+                  </Button>
+                )}
+              </div>
+            ) : team1Streamers.length > 0 || team2Streamers.length > 0 ? (
+              <div className="flex flex-col gap-2">
+                <div className="flex flex-wrap gap-2">
+                  {team1Streamers.map((streamer, i) => (
+                    <Button
+                      key={streamer}
+                      className={`bg-[#2a292e] font-semibold ${streamer && "cursor-pointer hover:bg-green-300"} ${selectedStreamerChat === streamer && "bg-green-400 hover:bg-green-300"}`}
+                      disabled={!streamer}
+                      title={streamer && streamer + "'s chat"}
+                      onClick={() => setSelectedStreamerChat(streamer)}
+                    >
+                      {streamer ? (
+                        <span className="text-white">{streamer}</span>
+                      ) : (
+                        <span className="opacity-40">
+                          {"Channel " + (i + 1)}
+                        </span>
+                      )}
+                    </Button>
+                  ))}
+
+                  {team1Streamers.length < 6 && (
+                    <Button
+                      onClick={() => {
+                        if (team1Streamers.length < 6) {
+                          setTeam1Streamers([...team1Streamers, ""]);
+                        }
+                      }}
+                      title="Add Channel"
+                    >
+                      <div className="flex items-center gap-1">
+                        <PlusIconSvg className="h-4 w-4" />
+                      </div>
+                    </Button>
+                  )}
                 </div>
-              </Button>
-            )}
-          </>
-        )}
+
+                <div className="border-1 border-[#302f35]" />
+
+                <div className="flex flex-wrap gap-2">
+                  {team2Streamers.map((streamer, i) => (
+                    <Button
+                      key={streamer}
+                      className={`bg-[#2a292e] font-semibold ${streamer && "cursor-pointer hover:bg-red-300"} ${selectedStreamerChat === streamer && "bg-red-400 hover:bg-red-300"}`}
+                      disabled={!streamer}
+                      title={streamer && streamer + "'s chat"}
+                      onClick={() => setSelectedStreamerChat(streamer)}
+                    >
+                      {streamer ? (
+                        <span className="text-white">{streamer}</span>
+                      ) : (
+                        <span className="opacity-40">
+                          {"Channel " + (i + 1)}
+                        </span>
+                      )}
+                    </Button>
+                  ))}
+
+                  {team2Streamers.length < 6 && (
+                    <Button
+                      onClick={() => {
+                        if (team2Streamers.length < 6) {
+                          setTeam2Streamers([...team2Streamers, ""]);
+                        }
+                      }}
+                      title="Add Channel"
+                    >
+                      <div className="flex items-center gap-1">
+                        <PlusIconSvg className="h-4 w-4" />
+                      </div>
+                    </Button>
+                  )}
+                </div>
+              </div>
+            ) : (
+              "cast"
+            ))}
+        </div>
       </div>
+
+      {showChat &&
+        !selectedExpandedStream &&
+        (team1Streamers.length > 0 || team2Streamers.length > 0) && (
+          <div className="flex justify-center">
+            <button
+              className="mx-2 my-1 flex w-full flex-col rounded-full bg-[#302f35] text-sm text-gray-400 hover:cursor-pointer hover:text-gray-300"
+              onClick={handleShowChatSelectOnClick}
+            >
+              <span>
+                {showChatSelect ? "Hide Chat Select" : "Show Chat Select"}
+              </span>
+            </button>
+          </div>
+        )}
 
       {showChat && (
         <iframe
           className="h-full w-full"
-          src={`https://www.twitch.tv/embed/${selectedExpandedStream || selectedStreamer}/chat?darkpopout&parent=${window.location.hostname}`}
+          src={`https://www.twitch.tv/embed/${selectedExpandedStream || selectedStreamerChat}/chat?darkpopout&parent=${window.location.hostname}`}
         />
       )}
     </div>
