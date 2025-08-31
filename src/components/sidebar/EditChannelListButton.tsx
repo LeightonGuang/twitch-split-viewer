db875411a819ec48c4510a551bc6ea077ca6e206
LeightonGuang <leighton.guang@icloud.com>
2025-08-31 01:49:30 +0100

    Feat: Show chat icon when in edit view

--- src/components/sidebar/EditChannelListButton.tsx
+++ src/components/sidebar/EditChannelListButton.tsx
@@ -1,21 +1,35 @@
-import { EditIconSvg } from "../../assets/Icons";
+import { ChatIconSvg, EditIconSvg } from "../../assets/Icons";
 
 const EditChannelListButton = ({
+  setShowSidebar,
   showEditChannelList,
   setshowEditChannelList,
 }: {
   showEditChannelList: boolean;
   setshowEditChannelList: React.Dispatch<React.SetStateAction<boolean>>;
-}) => (
-  <button
-    className={`p-2 leading-none hover:cursor-pointer hover:text-gray-400 ${showEditChannelList ? "text-[#9147ff]" : "text-gray-500"}`}
-    title="Edit Channel List"
-    onClick={() => {
-      setshowEditChannelList(!showEditChannelList);
-    }}
-  >
-    <EditIconSvg className="h-4 w-4" />
-  </button>
-);
+  setShowSidebar: React.Dispatch<React.SetStateAction<boolean>>;
+}) =>
+  showEditChannelList ? (
+    <button
+      className={`p-2 leading-none text-gray-500 hover:cursor-pointer hover:text-gray-400`}
+      title="Twitch Chat"
+      onClick={() => {
+        setshowEditChannelList(false);
+      }}
+    >
+      <ChatIconSvg className="h-4 w-4" />
+    </button>
+  ) : (
+    <button
+      className={`p-2 leading-none text-gray-500 hover:cursor-pointer hover:text-gray-400`}
+      title="Edit Channel List"
+      onClick={() => {
+        setShowSidebar(true);
+        setshowEditChannelList(true);
+      }}
+    >
+      <EditIconSvg className="h-4 w-4" />
+    </button>
+  );
 
 export default EditChannelListButton;
