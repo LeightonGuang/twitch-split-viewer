20771275cd3c37aaba97409c263834eea2e24196
LeightonGuang <leighton.guang@icloud.com>
2025-09-02 03:33:10 +0100

    Refactor: Update to use React.ComponentProps

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,26 +1,17 @@
 import React from "react";
 
-interface CustomButtonProps {
-  children?: React.ReactNode;
-  className?: string;
-  title?: string;
-  disabled?: boolean;
-  onClick: () => void;
-}
-
-const Button: React.FC<CustomButtonProps> = ({
+const Button = ({
   children,
   className,
-  title,
-  disabled,
-  onClick,
+  ...props
+}: React.ComponentProps<"button"> & {
+  children?: React.ReactNode;
+  className?: string;
 }) => {
   return (
     <button
+      {...props}
       className={`h-min cursor-pointer rounded-full bg-[#2a292e] px-3 py-2 text-sm leading-none font-semibold text-white hover:bg-[#302f35] ${className}`}
-      title={title}
-      disabled={disabled}
-      onClick={onClick}
     >
       {children}
     </button>
